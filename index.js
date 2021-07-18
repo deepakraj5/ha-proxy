13a565c9964ebaf26f8c95401ed4b06d6b327d3b
deepakraj5 <deepakatr4@gmail.com>
2021-07-18 19:40:30 +0530

    e flag

--- index.js
+++ index.js
@@ -3,13 +3,13 @@ require('dotenv').config()
 
 const app = express()
 
-const PORT = process.env.PORT
+const PORT = process.env.PORT || 3000
 
 app.use(express.json())
 
 
 app.get('/', (req, res) => {
-    res.send(`app running ${PORT}`)
+    res.send(`app running ${process.env.EFLAG}`)
 })
 
 
